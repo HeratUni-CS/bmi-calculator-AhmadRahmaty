096006fda37cc7f79884e2d0aa2ba166e1fc62d9
AhmadRahmaty <rahmaty.ahmad.2020@gmail.com>
2023-05-28 13:22:41 +0430

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,23 @@
 import 'package:flutter/material.dart';
 
+import 'screens/input_page.dart';
+
 void main() => runApp(BMICalculator());
 
-class BMICalculator extends StatelessWidget {
+class BMICalculator
+    extends StatelessWidget {
+  const BMICalculator({Key? key})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InputPage(),
+      theme: ThemeData.dark().copyWith(
+        appBarTheme: AppBarTheme(backgroundColor: Color(0xFF0A0D22)),
+          scaffoldBackgroundColor:
+              Color(0xFF0A0D22),
+         ),
+      home: inputPage(),
     );
   }
 }
 
-class InputPage extends StatefulWidget {
-  @override
-  State<InputPage> createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
